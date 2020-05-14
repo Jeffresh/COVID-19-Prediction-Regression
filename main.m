c81e439fa5bb54c71d5b71702076f62d06abeb1e
Jeff <jeff.uca@gmail.com>
2020-05-14 21:25:05 +0200

    :sparkles: prediction over china data

--- main.m
+++ main.m
@@ -2,12 +2,27 @@ clear all, close all;
 
 data = readtable("assets/COVID-19.csv");
 
-%%
+ 
+active_res_china = getData(data, "China",97/147);
+[active_res_spain, confirmed_res, death_res, recovered_res, date] = getData(data, "Spain");
 
-[Xtrain, Ytrain] = build_dataset(data,"China"); 
+x = dates2num(date);
+y = active_res_china;
 
-[Xtest, Ytest] = build_dataset(data,"Spain", 97/147,1); 
 
+yp = log((y./x));
+A = [x.^2, x ,ones(size(x))];
 
+sol = inv(A'*A)*(A'*yp); 
+
+C = sol(1); B = sol(2); A = exp(sol(3));
+
+forecast = 30;
+
+newdate = getVectorDates(date,forecast);
+x=[1:x(end)+forecast];
+
+plot(date,active_res_china,'b'), hold on
+plot(newdate, A*x.*exp(B*x + C*x.^2),'g')
 
 
